cf4aeddb10d93aa8f00e4b01c2264f50822a12cd
unknown <Rajiv>
2021-09-11 13:53:59 +0530

    final commit

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+
+from os import pardir
+from django.contrib import admin
+from django.urls import path,include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include('main.urls'))
+
+]
